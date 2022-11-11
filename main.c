5fa599ea582ea847c65c83d4d7f7cbb6ff6c6ba6
chengxuyuan <chengxuyuan1998@gmail.com>
2022-11-11 10:31:13 +0100

    add indirect threaded interpreter

--- main.c
+++ main.c
@@ -7,7 +7,8 @@
 #include "gen.c"
 
 // op codes
-enum {
+enum
+{
     HALT,
     CLRA,
     INC3A,
@@ -20,8 +21,10 @@ int str2i(const char *str, char split, char **endptr)
 {
     int ret;
     ret = strtol(str, endptr, 10);
-    if (*endptr != NULL) {
-        if (**endptr == split) {
+    if (*endptr != NULL)
+    {
+        if (**endptr == split)
+        {
             *endptr += 1;
         }
     }
@@ -29,23 +32,188 @@ int str2i(const char *str, char split, char **endptr)
     return ret;
 }
 
-int interpreter(char *buf, int size, int a, int l) {
-    for (int i = 0; i < size; ++i) {
-        switch (buf[i]) {
-            case HALT:  return a;
-            case CLRA:  a = 0;  break;
-            case INC3A: a += 3; break;
-            case DECA:  a--;    break;
-            case SELA:  l = a;  break;
-            case BACK7: l--;    if (l > 0)  i -= 7; break;
+int threaded_interpreter(char *buf, int size, int a, int l)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        switch (buf[i])
+        {
+        case HALT:
+            return a;
+        case CLRA:
+            a = 0;
+            break;
+        case INC3A:
+            a += 3;
+            break;
+        case DECA:
+            a--;
+            break;
+        case SELA:
+            l = a;
+            break;
+        case BACK7:
+            l--;
+            if (l > 0)
+                i -= 7;
+            break;
         }
     }
     return a;
 }
 
-int main(int argc, char **argv) {
-    if (argc < 2) {
-        printf("Usage: ./main + probability(e.g. 00100)\n");
+int indirect_threaded_interpreter(char *buf, int size, int a, int l)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        switch (buf[i])
+        {
+        case HALT:
+            return a;
+        case CLRA:
+            a = 0;
+            if (buf[i + 1] == HALT)
+                return a;
+            switch (buf[i + 1])
+            {
+            case INC3A:
+                a = 3;
+                break;
+            case DECA:
+                a = -1;
+                break;
+            case SELA:
+                l = a;
+                break;
+            case BACK7:
+                l--;
+                if (l > 0)
+                    i -= 7;
+                break;
+            default:
+                break;
+            }
+            i++;
+            break;
+        case INC3A:
+            a += 3;
+            if (buf[i + 1] == HALT)
+                return a;
+            switch (buf[i + 1])
+            {
+            case CLRA:
+                a = 0;
+                break;
+            case INC3A:
+                a += 3;
+                break;
+            case DECA:
+                a--;
+                break;
+            case SELA:
+                l = a;
+                break;
+            case BACK7:
+                l--;
+                if (l > 0)
+                    i -= 7;
+                break;
+            default:
+                break;
+            }
+            i++;
+            break;
+        case DECA:
+            a--;
+            if (buf[i + 1] == HALT)
+                return a;
+            switch (buf[i + 1])
+            {
+            case CLRA:
+                a = 0;
+                break;
+            case INC3A:
+                a += 3;
+                break;
+            case DECA:
+                a--;
+                break;
+            case SELA:
+                l = a;
+                break;
+            case BACK7:
+                l--;
+                if (l > 0)
+                    i -= 7;
+                break;
+            default:
+                break;
+            }
+            i++;
+            break;
+        case SELA:
+            l = a;
+            if (buf[i + 1] == HALT)
+                return a;
+            switch (buf[i + 1])
+            {
+            case CLRA:
+                a = 0;
+                break;
+            case INC3A:
+                a += 3;
+                break;
+            case DECA:
+                a--;
+                break;
+            case SELA:
+                l = a;
+                break;
+            case BACK7:
+                l--;
+                if (l > 0)
+                    i -= 7;
+                break;
+            default:
+                break;
+            }
+            i++;
+            break;
+        case BACK7:
+            l--;
+            if (l > 0)
+                i -= 7;
+            if (buf[i + 1] == HALT)
+                return a;
+            switch (buf[i + 1])
+            {
+            case CLRA:
+                a = 0;
+                break;
+            case INC3A:
+                a += 3;
+                break;
+            case DECA:
+                a--;
+                break;
+            case SELA:
+                l = a;
+                break;
+            default:
+                break;
+            }
+            i++;
+            break;
+        }
+    }
+    return a;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        printf("Usage: ./main + probability(e.g. 0-0-1-0-0)\n");
         exit(1);
     }
     // time
@@ -55,21 +223,26 @@ int main(int argc, char **argv) {
     int32_t a, l;
 
     int size = 50000;
-    char *buf = (char *) malloc(sizeof(char) * size);
+    char *buf = (char *)malloc(sizeof(char) * size);
     int seed = 1;
     int prob[5];
     char *end = NULL;
     char *p_shift = argv[1];
 
     // decode command line parameters
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 5; i++)
+    {
         prob[i] = str2i(p_shift, '-', &end);
         p_shift = end;
     }
-    start_t = clock();
     init(buf, size, prob, seed, &a, &l);
+    start_t = clock();
+    a = threaded_interpreter(buf, size, a, l);
     end_t = clock();
-    a = interpreter(buf, size, a, l);
-    printf("%d instructions distributed in %s took %lu cpu clocks\n", size, argv[1], (end_t - start_t));
+    printf("         threaded: %d instructions distributed in %s took %lu cpu clocks value a: %d\n", size, argv[1], (end_t - start_t), a);
+    start_t = clock();
+    a = indirect_threaded_interpreter(buf, size, a, l);
+    end_t = clock();
+    printf("indirect threaded: %d instructions distributed in %s took %lu cpu clocks value a: %d\n", size, argv[1], (end_t - start_t), a);
     return 0;
 }
