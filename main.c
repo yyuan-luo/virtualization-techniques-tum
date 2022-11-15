01bd625d7c6279277440667351534be53c22624a
chengxuyuan <chengxuyuan1998@gmail.com>
2022-11-15 20:06:11 +0100

    code refactor

--- main.c
+++ main.c
@@ -32,11 +32,11 @@ int str2i(const char *str, char split, char **endptr)
     return ret;
 }
 
-int threaded_interpreter(char *buf, int size, int a, int l)
+int threaded_interpreter(char *instructions, int size, int a, int l)
 {
     for (int i = 0; i < size; ++i)
     {
-        switch (buf[i])
+        switch (instructions[i])
         {
         case HALT:
             return a;
@@ -62,10 +62,10 @@ int threaded_interpreter(char *buf, int size, int a, int l)
     return a;
 }
 
-int indirect_threaded_interpreter(char *buf, int size, int a, int l)
+int indirect_threaded_interpreter(char *instructions, int size, int a, int l)
 {
     static void *dispatch_table[] = {&&DO_HALT, &&DO_CLRA, &&DO_INC3A, &&DO_DECA, &&DO_BACK7};
-#define DISPATCH() goto *dispatch_table[buf[pc++]]
+#define DISPATCH() goto *dispatch_table[instructions[pc++]]
 
     int pc = 0;
     while (1)
@@ -97,7 +97,7 @@ int main(int argc, char **argv)
 {
     if (argc < 2)
     {
-        printf("Usage: ./main + probability(e.g. 0-0-1-0-0)\n");
+        printf("Usage: ./main + iteration + probability(e.g. ./main 5 0-1-0-0-0)\n");
         exit(1);
     }
     // time
@@ -107,11 +107,12 @@ int main(int argc, char **argv)
     int32_t a, l;
 
     int size = 50000;
-    char *buf = (char *)malloc(sizeof(char) * size);
+    char *instructions = (char *)malloc(sizeof(char) * size);
     int seed = 1;
     int prob[5];
     char *end = NULL;
-    char *p_shift = argv[1];
+    char *p_shift = argv[2];
+    int iteration = atoi(argv[1]);
 
     // decode command line parameters
     for (int i = 0; i < 5; i++)
@@ -119,14 +120,27 @@ int main(int argc, char **argv)
         prob[i] = str2i(p_shift, '-', &end);
         p_shift = end;
     }
-    init(buf, size, prob, seed, &a, &l);
-    start_t = clock();
-    a = threaded_interpreter(buf, size, a, l);
-    end_t = clock();
-    printf("         threaded: %d instructions distributed in %s took %lu cpu clocks value a: %d\n", size, argv[1], (end_t - start_t), a);
-    start_t = clock();
-    a = indirect_threaded_interpreter(buf, size, a, l);
-    end_t = clock();
-    printf("indirect threaded: %d instructions distributed in %s took %lu cpu clocks value a: %d\n", size, argv[1], (end_t - start_t), a);
+    // init instructions
+    init(instructions, size, prob, seed, &a, &l);
+
+    for (int i = 0; i < iteration; i++)
+    {
+        start_t = clock();
+        a = threaded_interpreter(instructions, size, a, l);
+        end_t = clock();
+        printf("threaded: %d instructions distributed in %s took %lu cpu clocks, final value of a: %d\n", size, argv[1], (end_t - start_t), a);
+        a = l = 0;
+    }
+    printf("/*----------------------------------------------------------------------------------------------*/\n");
+    for (int i = 0; i < iteration; i++)
+    {
+        start_t = clock();
+        a = indirect_threaded_interpreter(instructions, size, a, l);
+        end_t = clock();
+        printf("indirect threaded: %d instructions distributed in %s took %lu cpu clocks, final value of a: %d\n", size, argv[1], (end_t - start_t), a);
+        a = l = 0;
+    }
+    
+
     return 0;
 }
